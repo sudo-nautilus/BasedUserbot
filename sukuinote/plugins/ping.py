7574ec4821f081996f627138cc32f271ce11b5a0
blank X <theblankx@protonmail.com>
2021-03-10 15:52:43 +0700

    change some things

--- sukuinote/plugins/ping.py
+++ sukuinote/plugins/ping.py
@@ -2,19 +2,20 @@ import time
 from pyrogram import Client, filters
 from .. import config, help_dict, log_errors, public_log_errors, log_ring
 
+strings = {
+    'ping': 'Pong!',
+    'pong': 'Ping!'
+}
+
 @Client.on_message(~filters.forwarded & ~filters.sticker & ~filters.via_bot & ~filters.edited & filters.me & filters.command(['ping', 'pong'], prefixes=config['config']['prefixes']))
 @log_errors
 @public_log_errors
 async def ping_pong(client, message):
-    strings = {
-        'ping': 'Pong!',
-        'pong': 'Ping!'
-    }
     text = strings[message.command[0].lower()]
     start = time.time()
-    reply = await message.reply_text(text)
+    reply = await message.reply_text(f'{text}\n<b>Log Ring:</b> {len(log_ring)}/{log_ring.maxlen}')
     end = time.time()
-    await reply.edit_text(f'{text}\n<i>{round((end-start)*1000)}ms</i>\n<b>Log Ring:</b> <i>{len(log_ring)}/{log_ring.maxlen}</i>')
+    await reply.edit_text(f'{text} <i>{round((end-start)*1000)}ms</i>\n<b>Log Ring:</b> {len(log_ring)}/{log_ring.maxlen}')
 
 help_dict['ping'] = ('Ping',
 '''{prefix}ping - Pong!
