01e86fd948129de663c35ac295d8e3e41b27f6ad
blank X <theblankx@protonmail.com>
2021-05-07 20:37:28 +0700

    Loli replies to the replied message if avaliable

--- sukuinote/plugins/loli.py
+++ sukuinote/plugins/loli.py
@@ -11,14 +11,17 @@ async def loli(client, message):
     query = ' '.join(message.command)
     results = await client.get_inline_bot_results(bot.username or bot.id, query)
     result = results.results[0]
+    to_reply = message
+    if not getattr(message.reply_to_message, 'empty', True):
+        to_reply = message.reply_to_message
     if result.type != 'photo':
-        await message.reply_text({'message': result.send_message.message, 'entities': result.send_message.entities}, parse_mode='through')
+        await to_reply.reply_text({'message': result.send_message.message, 'entities': result.send_message.entities}, parse_mode='through')
         return
     photo = Photo._parse(client, result.photo)
     try:
-        await message.reply_cached_media(photo.file_id, caption={'message': result.send_message.message, 'entities': result.send_message.entities}, parse_mode='through')
+        await to_reply.reply_cached_media(photo.file_id, caption={'message': result.send_message.message, 'entities': result.send_message.entities}, parse_mode='through')
     except Forbidden:
-        await message.reply_text({'message': result.send_message.message, 'entities': result.send_message.entities}, parse_mode='through')
+        await to_reply.reply_text({'message': result.send_message.message, 'entities': result.send_message.entities}, parse_mode='through')
 
 help_dict['loli'] = ('Loli',
 '''{prefix}loli <i>[keywords]</i> - Gets a possibly nsfw image of a loli, thanks to lolicon.app
