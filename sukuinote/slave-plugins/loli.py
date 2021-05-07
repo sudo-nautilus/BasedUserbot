54f2579567c06f57e5ca19f0c90ea9d17b6a260c
blank X <theblankx@protonmail.com>
2021-05-07 20:44:12 +0700

    Fix typo

--- sukuinote/slave-plugins/loli.py
+++ sukuinote/slave-plugins/loli.py
@@ -18,7 +18,7 @@ async def loli(client, inline_query):
             mode = 1
     else:
         mode = 2
-    async with session.get(f'https://api.lolicon.app/setu?num=1&r18={mode}&keyword={urlencode(match.group(2).strip())}&apikey={urlencode(config["config"].get("lollicon_api", ""))}') as resp:
+    async with session.get(f'https://api.lolicon.app/setu?num=1&r18={mode}&keyword={urlencode(match.group(2).strip())}&apikey={urlencode(config["config"].get("lolicon_api", ""))}') as resp:
         data = await resp.json()
     if data['code'] or data['msg']:
         item = InlineQueryResultArticle(f'Error {data["code"]}', InputTextMessageContent(f'<b>{html.escape(str(data["code"]))}:</b> {html.escape(data["msg"])}', parse_mode='html'), description=data['msg'] or None)
